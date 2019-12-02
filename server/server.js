c7ff51488c1e4e14ea83ec0ed205d0d509bff5b4
anjorlar <adeanjola@gmail.com>
2019-12-02 15:42:32 +0100

    schema validation

--- server/server.js
+++ server/server.js
@@ -5,20 +5,38 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 let Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        require: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-let newTodo = new Todo({
-    text: "cook dinner"
+// let newTodo = new Todo({
+//     text: "cook dinner"
+// });
+
+// let newTodo = new Todo({
+//     text: "order lunch",
+//     completed: false
+// });
+
+// newTodo.save().then((res) => {
+//     console.log('todo saved', res)
+// }).catch((e) => console.log('unable to save todo', e))
+
+let anotherTodo = new Todo({
+    text: '   new todo       '
 });
 
-newTodo.save().then((res) => {
-    console.log('todo saved', res)
-}).catch((e) => console.log('unable to save todo', e))
+anotherTodo.save().then((res) => {
+    console.log(JSON.stringify(res, undefined, 2))
+}).catch((err) => console.log("unable to save", err))
