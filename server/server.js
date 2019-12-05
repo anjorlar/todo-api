6d9f565ee3545890ee8031b7fd1942cdc1c5ae7e
anjorlar <adeanjola@gmail.com>
2019-12-05 10:52:23 +0100

    implemented getting by id

--- server/server.js
+++ server/server.js
@@ -19,21 +19,24 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
     let todo = new Todo({
-
         text: req.body.text
     });
     todo.save().then((doc) => {
         res.send(doc)
     }).catch((e) => res.status(400).send(e))
-})
+});
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({ todos })
     }).catch((e) => res.status(400).status(e))
-})
+});
+
+app.get('/todos/:id', (req, res) => {
+    res.send(req.params);
+});
 app.listen(3000, () => {
     console.log('app listening on port 3000')
-})
+});
 
 module.exports = app;
