58c1fcf63dcf52903a0cbb2464d21daaab606af1
anjorlar <adeanjola@gmail.com>
2019-12-20 19:03:35 +0100

    comments

---
+++ playground/terminal.js
@@ -0,0 +1,185 @@
+// Last login: Thu Dec 12 10:43:35 on ttys005
+// Users-MBP:~ user$ git clone https://github.com/gbnlrepo/movement-service.git
+// Cloning into 'movement-service'...
+// remote: Enumerating objects: 63, done.
+// remote: Counting objects: 100% (63/63), done.
+// remote: Compressing objects: 100% (44/44), done.
+// remote: Total 360 (delta 26), reused 38 (delta 16), pack-reused 297
+// Receiving objects: 100% (360/360), 114.61 KiB | 371.00 KiB/s, done.
+// Resolving deltas: 100% (169/169), done.
+// Users-MBP:~ user$ code .
+// Users-MBP:~ user$ ls
+// Applications		Movies			dump.rdb
+// Desktop			Music			mongo
+// Documents		Pictures		mongo-data
+// Downloads		Postman			movement-service
+// Library			Public
+// Users-MBP:~ user$ rm movement-service
+// rm: movement-service: is a directory
+// Users-MBP:~ user$ rm -R movement-service
+// override r--r--r--  user/staff for movement-service/.git/objects/pack/pack-9b159ef749ca663fdc1a6e3e27fef8871bbf45a1.idx? 
+// override r--r--r--  user/staff for movement-service/.git/objects/pack/pack-9b159ef749ca663fdc1a6e3e27fef8871bbf45a1.pack? ^C
+// Users-MBP:~ user$ LS
+// Applications		Movies			dump.rdb
+// Desktop			Music			mongo
+// Documents		Pictures		mongo-data
+// Downloads		Postman			movement-service
+// Library			Public
+// Users-MBP:~ user$ ls -a
+// .					.pgadmin
+// ..					.rdbtools
+// .3T					.rediscli_history
+// .CFUserTextEncoding			.ssh
+// .DS_Store				.viminfo
+// .Trash					.vscode
+// .bash_history				Applications
+// .bash_sessions				Desktop
+// .config					Documents
+// .dbshell				Downloads
+// .gitconfig				Library
+// .local					Movies
+// .mongorc.js				Music
+// .netrc					Pictures
+// .node-gyp				Postman
+// .node_repl_history			Public
+// .npm					dump.rdb
+// .pgAdmin4.9731491707523589030.addr	mongo
+// .pgAdmin4.9731491707523589030.log	mongo-data
+// .pgAdmin4.startup.log			movement-service
+// Users-MBP:~ user$ rm -R movement-service
+// override r--r--r--  user/staff for movement-service/.git/objects/pack/pack-9b159ef749ca663fdc1a6e3e27fef8871bbf45a1.idx? 
+// override r--r--r--  user/staff for movement-service/.git/objects/pack/pack-9b159ef749ca663fdc1a6e3e27fef8871bbf45a1.pack? 
+// rm: movement-service/.git/objects/pack: Directory not empty
+// rm: movement-service/.git/objects: Directory not empty
+// rm: movement-service/.git: Directory not empty
+// rm: movement-service: Directory not empty
+// Users-MBP:~ user$ ls
+// Applications		Movies			dump.rdb
+// Desktop			Music			mongo
+// Documents		Pictures		mongo-data
+// Downloads		Postman			movement-service
+// Library			Public
+// Users-MBP:~ user$ cd movement-service
+// Users-MBP:movement-service user$ ls -a
+// .	..	.git
+// Users-MBP:movement-service user$ ls
+// Users-MBP:movement-service user$ cd ..
+// Users-MBP:.Trash user$ cd
+// Users-MBP:~ user$ ls
+// Applications	Downloads	Music		Public		mongo-data
+// Desktop		Library		Pictures	dump.rdb
+// Documents	Movies		Postman		mongo
+// Users-MBP:~ user$ ls -a
+// .					.pgadmin
+// ..					.rdbtools
+// .3T					.rediscli_history
+// .CFUserTextEncoding			.ssh
+// .DS_Store				.viminfo
+// .Trash					.vscode
+// .bash_history				Applications
+// .bash_sessions				Desktop
+// .config					Documents
+// .dbshell				Downloads
+// .gitconfig				Library
+// .local					Movies
+// .mongorc.js				Music
+// .netrc					Pictures
+// .node-gyp				Postman
+// .node_repl_history			Public
+// .npm					dump.rdb
+// .pgAdmin4.9731491707523589030.addr	mongo
+// .pgAdmin4.9731491707523589030.log	mongo-data
+// .pgAdmin4.startup.log
+// Users-MBP:~ user$ cd Desktop
+// Users-MBP:Desktop user$ ls
+// Screenshot 2019-10-14 at 5.55.07 PM.png
+// Screenshot 2019-10-15 at 4.21.16 PM.png
+// Screenshot 2019-12-04 at 11.22.45 AM (2).png
+// Screenshot 2019-12-04 at 11.32.24 AM (2).png
+// Screenshot 2019-12-04 at 11.32.24 AM.png
+// Screenshot 2019-12-04 at 11.32.31 AM (2).png
+// Screenshot 2019-12-04 at 11.32.31 AM.png
+// Screenshot 2019-12-04 at 11.32.37 AM (2).png
+// Screenshot 2019-12-04 at 11.32.37 AM.png
+// datafromduration.js
+// raise-order-service
+// Users-MBP:Desktop user$ git clone https://github.com/gbnlrepo/movement-service.git
+// Cloning into 'movement-service'...
+// remote: Enumerating objects: 63, done.
+// remote: Counting objects: 100% (63/63), done.
+// remote: Compressing objects: 100% (44/44), done.
+// remote: Total 360 (delta 26), reused 38 (delta 16), pack-reused 297
+// Receiving objects: 100% (360/360), 114.61 KiB | 408.00 KiB/s, done.
+// Resolving deltas: 100% (169/169), done.
+// Users-MBP:Desktop user$ code .
+// Users-MBP:Desktop user$ ls
+// Screenshot 2019-10-14 at 5.55.07 PM.png
+// Screenshot 2019-10-15 at 4.21.16 PM.png
+// Screenshot 2019-12-04 at 11.22.45 AM (2).png
+// Screenshot 2019-12-04 at 11.32.24 AM (2).png
+// Screenshot 2019-12-04 at 11.32.24 AM.png
+// Screenshot 2019-12-04 at 11.32.31 AM (2).png
+// Screenshot 2019-12-04 at 11.32.31 AM.png
+// Screenshot 2019-12-04 at 11.32.37 AM (2).png
+// Screenshot 2019-12-04 at 11.32.37 AM.png
+// datafromduration.js
+// raise-order-service
+// Users-MBP:Desktop user$ git clone https://github.com/gbnlrepo/movement-service.git
+// Cloning into 'movement-service'...
+// remote: Enumerating objects: 63, done.
+// remote: Counting objects: 100% (63/63), done.
+// remote: Compressing objects: 100% (44/44), done.
+// remote: Total 360 (delta 26), reused 38 (delta 16), pack-reused 297
+// Receiving objects: 100% (360/360), 114.61 KiB | 499.00 KiB/s, done.
+// Resolving deltas: 100% (169/169), done.
+// Users-MBP:Desktop user$ code .
+// Users-MBP:Desktop user$ ls
+// Screenshot 2019-10-14 at 5.55.07 PM.png
+// Screenshot 2019-10-15 at 4.21.16 PM.png
+// Screenshot 2019-12-04 at 11.22.45 AM (2).png
+// Screenshot 2019-12-04 at 11.32.24 AM (2).png
+// Screenshot 2019-12-04 at 11.32.24 AM.png
+// Screenshot 2019-12-04 at 11.32.31 AM (2).png
+// Screenshot 2019-12-04 at 11.32.31 AM.png
+// Screenshot 2019-12-04 at 11.32.37 AM (2).png
+// Screenshot 2019-12-04 at 11.32.37 AM.png
+// datafromduration.js
+// raise-order-service
+// Users-MBP:Desktop user$ git clone https://github.com/gbnlrepo/movement-service.git
+// Cloning into 'movement-service'...
+// remote: Enumerating objects: 63, done.
+// remote: Counting objects: 100% (63/63), done.
+// remote: Compressing objects: 100% (44/44), done.
+// remote: Total 360 (delta 26), reused 38 (delta 16), pack-reused 297
+// Receiving objects: 100% (360/360), 114.61 KiB | 138.00 KiB/s, done.
+// Resolving deltas: 100% (169/169), done.
+// Users-MBP:Desktop user$ git clone https://github.com/gbnlrepo/movement-service.git
+// Cloning into 'movement-service'...
+// remote: Enumerating objects: 63, done.
+// remote: Counting objects: 100% (63/63), done.
+// remote: Compressing objects: 100% (44/44), done.
+// remote: Total 360 (delta 26), reused 38 (delta 16), pack-reused 297
+// Receiving objects: 100% (360/360), 114.61 KiB | 505.00 KiB/s, done.
+// Resolving deltas: 100% (169/169), done.
+// Users-MBP:Desktop user$ code .
+// Users-MBP:Desktop user$ 
+//   [Restored 14 Dec 2019 at 12:02:08 PM]
+// Last login: Sat Dec 14 12:02:08 on ttys002
+// Restored session: Fri Dec 13 23:28:23 WAT 2019
+// Users-MBP:Desktop user$ code .
+// Users-MBP:Desktop user$ code .
+// Users-MBP:Desktop user$ code .
+// Users-MBP:Desktop user$ code .
+// Users-MBP:Desktop user$ code .
+// Users-MBP:Desktop user$ 
+//   [Restored 17 Dec 2019 at 10:47:47 AM]
+// Last login: Tue Dec 17 10:47:46 on ttys002
+// Restored session: Tue Dec 17 10:45:01 WAT 2019
+// Users-MBP:Desktop user$ 
+//   [Restored 17 Dec 2019 at 10:51:38 AM]
+// Last login: Tue Dec 17 10:51:38 on ttys001
+// Users-MBP:Desktop user$ code .
+// Users-MBP:Desktop user$ code .
+// Users-MBP:Desktop user$ code .
+// Users-MBP:Desktop user$ code .
+// Users-MBP:Desktop user$ 
