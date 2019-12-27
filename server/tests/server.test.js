895dc88254901deed3e855c9910e3608ca938fcc
anjorlar <adeanjola@gmail.com>
2019-12-27 19:48:47 +0100

    test private route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,7 +6,7 @@ const Todo = require('../models/todo');
 const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
 beforeEach(populateTodos);
-// beforeEach(populateUsers);
+beforeEach(populateUsers);
 
 describe("Post /todos", () => {
     it('should create a new todo', (done) => {
@@ -164,6 +164,56 @@ describe('Patch todo/:id', () => {
     });
 });
 
+describe('Get /users/me', () => {
+    it('should return user if authenticated', (done) => {
+        request(app)
+            .get('/users/me')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body._id).toBe(users[0]._id.toHexString())
+                expect(res.body.email).toBe(users[0].email);
+            })
+            .end(done);
+    });
+    it('should return 401 if user isn\'t authenticated', (done) => {
+        request(app)
+            .get('/users/me')
+            .expect(401)
+            .expect((res) => {
+                // console.log(res.body)
+                expect(res.body.message).toBe("no user found")
+            })
+            .end(done);
+    })
+})
+
+describe('Post /users', () => {
+    it('should create a user', (done) => {
+        let email = 'ali@example.com'
+        let password = '123mnb!'
+
+        request(app)
+            .post('/users')
+            .send({ email, password })
+            .expect(200)
+            .expect((res) => {
+                console.log(res)
+                expect(res.headers['x-auth']).toBeTruthy();
+                expect(res.body._id).toBeTruthy();
+                expect(res.body.email).toBe(email);
+            })
+            .end(done)
+    });
+
+    // it(`should return validation errors if request is invalid`, (done) => {
+
+    // })
+
+    // it(`should not create user if email is in use`, (done) => {
+
+    // })
+})
 // git commit -a -m 'adde new test case and route' correct command
 // git commit -am 'added new file' correct command
 
