7575d75bdac9ea763831768077d0481f6229fd03
anjorlar <adeanjola@gmail.com>
2020-01-18 16:42:53 +0100

    file update

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 let env = process.env.NODE_ENV || 'development';
 
 if (env === 'development' || env === 'test') {
-    let config = require('./config.json')
+    let config = require('./config.json');
 
     let envConfig = config[env];
     Object.keys(envConfig).forEach((key) => {
