95017ba38f3b93af809b90f3f120d40fe8a44e75
anjorlar <adeanjola@gmail.com>
2020-03-30 15:42:42 +0100

    file changes

--- server/models/user.js
+++ server/models/user.js
@@ -113,7 +113,7 @@ UserSchema.pre('save', function (next) {
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(user.password, salt, (err, hash) => {
                 user.password = hash;
-                next()
+                next();
             });
         });
     } else {
